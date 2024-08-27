ae8cf57fd413fe7d6f7c8b6a0cdb566f0224bd65
itguytrades <itguytrades@proton.me>
2024-08-27 16:35:30 -0700

    3. Testing Contracts

---
+++ test/token.js
@@ -0,0 +1,39 @@
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
+
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), 'ether')
+}
+
+describe('Token', () => {
+	let token
+	beforeEach(async () => {
+	const Token = await ethers.getContractFactory('Token')
+  	token = await Token.deploy('Dapp University', 'DAPP', '1000000')
+	})
+
+
+
+	describe('Deployment', () => {
+		const name = 'Dapp University'
+		const symbol = 'DAPP'
+		const decimals = '18'
+		const totalSupply = tokens(1000000)
+
+		  it('Has correct name', async () => {
+		  	expect(await token.name()).to.equal(name)
+		  })
+		  it('Has correct symbol', async () => {
+		  	expect(await token.symbol()).to.equal(symbol)
+		  })
+		  it('Has correct decimals', async () => {
+		  	expect(await token.decimals()).to.equal(decimals)
+		  })
+		  it('Has correct total Supply', async () => {
+		  	expect(await token.totalSupply()).to.equal(totalSupply)
+		  })
+
+	})
+
+
+})
