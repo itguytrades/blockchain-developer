5b76828f7863d4bbdc7d93d95e50460bb679a0b7
itguytrades <itguytrades@proton.me>
2024-08-27 18:48:45 -0700

    4.Token Balances

--- test/token.js
+++ test/token.js
@@ -5,11 +5,16 @@ const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
+
 describe('Token', () => {
-	let token
+	let token, accounts, deployer
+
 	beforeEach(async () => {
-	const Token = await ethers.getContractFactory('Token')
+		const Token = await ethers.getContractFactory('Token')
   	token = await Token.deploy('Dapp University', 'DAPP', '1000000')
+
+	  accounts = await ethers.getSigners()
+	  deployer = accounts[0].address
 	})
 
 
@@ -20,20 +25,26 @@ describe('Token', () => {
 		const decimals = '18'
 		const totalSupply = tokens(1000000)
 
-		  it('Has correct name', async () => {
-		  	expect(await token.name()).to.equal(name)
-		  })
-		  it('Has correct symbol', async () => {
-		  	expect(await token.symbol()).to.equal(symbol)
-		  })
-		  it('Has correct decimals', async () => {
-		  	expect(await token.decimals()).to.equal(decimals)
-		  })
-		  it('Has correct total Supply', async () => {
-		  	expect(await token.totalSupply()).to.equal(totalSupply)
-		  })
+	  it('Has correct name', async () => {
+	  	expect(await token.name()).to.equal(name)
+	  })
+
+	  it('Has correct symbol', async () => {
+	  	expect(await token.symbol()).to.equal(symbol)
+	  })
+
+	  it('Has correct decimals', async () => {
+	  	expect(await token.decimals()).to.equal(decimals)
+	  })
+
+	  it('Has correct total Supply', async () => {
+	  	expect(await token.totalSupply()).to.equal(totalSupply)
+	  })
+
+	  it('assigns total supply to deployer', async () => {
+	  	expect(await token.balanceOf(deployer)).to.equal(totalSupply)
+	  })
 
 	})
-
 
 })
